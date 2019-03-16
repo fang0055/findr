1cce5b038c299f33806b8485335d96d44866b428
Link Fang <fang0055@algonquinlive.com>
2019-03-16 17:03:09 -0400

    tested google map

--- www/js/index.js
+++ www/js/index.js
@@ -1,46 +1,20 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+let map;
+document.addEventListener("DOMContentLoaded", () => {
+    let s = document.createElement("script");
+    document.head.appendChild(s);
+    s.addEventListener("load", () => {
+        //script has loaded
+        console.log("script has loaded");
+        map = new google.maps.Map(document.getElementById("map"), {
+            center: {
+                lat: 45.3496711,
+                lng: -75.7569551
+            },
+            zoom: 16,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+    });
+    s.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyBksheP1AaeyshQdY1ZWlgk73ZicfBl3iE`;
+});
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
-};
-
-app.initialize();
