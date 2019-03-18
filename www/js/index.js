401fc55a02f05df7a5f658ce0d4604524539361f
Link Fang <fang0055@algonquinlive.com>
2019-03-18 15:38:59 -0400

    tested marker

--- www/js/index.js
+++ www/js/index.js
@@ -1,20 +1,73 @@
-let map;
-document.addEventListener("DOMContentLoaded", () => {
-    let s = document.createElement("script");
-    document.head.appendChild(s);
-    s.addEventListener("load", () => {
-        //script has loaded
-        console.log("script has loaded");
-        map = new google.maps.Map(document.getElementById("map"), {
-            center: {
-                lat: 45.3496711,
-                lng: -75.7569551
-            },
-            zoom: 16,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        });
-    });
-    s.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyBksheP1AaeyshQdY1ZWlgk73ZicfBl3iE`;
-});
+let app = {
+    map: "",
+    // APIKey: "js?key=AIzaSyBksheP1AaeyshQdY1ZWlgk73ZicfBl3iE",
+    
+    latVal: 60,
+    lngVal: -75,
+    maker: "",
 
+    init: function (){
+        app.ready();
+    },
 
+    ready: function() {
+        const opts = {
+            enableHighAccuracy: true,
+            timeout: 20000,
+            maximumAge: 1000 * 60 * 60 * 24
+            };
+        // console.log("hello?")
+        navigator.geolocation.getCurrentPosition(getPos, failPos, opts);
+
+        function getPos(position){
+            // console.log("what?????");
+            let coords = position.coords;
+            app.latVal = coords.latitude;
+            app.lngVal = coords.longitude;
+
+            app.map = new google.maps.Map(document.querySelector(".map"), {
+                center: {
+                    lat: app.latVal,
+                    lng: app.lngVal
+                },
+                zoom: 15,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            });
+
+            let infowindow = new google.maps.InfoWindow({
+                content: "test"
+            });
+
+            google.maps.event.addListener(app.map, 'click', function(ev) {
+                app.marker = new google.maps.Marker({
+                    animation: google.maps.Animation.DROP,
+                    position: ev.latLng,
+                    map: app.map,
+                    title: "This is the mouse over text"
+                });
+    
+                google.maps.event.addListener(app.marker, 'click', function(ev) {
+                    infowindow.open(app.map, app.marker);
+                });
+            });
+        }
+
+        function failPos(err){
+            console.log(err.code + err.message);
+            app.map = new google.maps.Map(document.querySelector(".map"), {
+                center: {
+                    lat: 45.3496711,
+                    lng: -75.7569551
+                },
+                zoom: 15,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            });
+        }
+    }
+}
+
+if ("cordova" in window) {
+    document.addEventListener("deviceready", app.init);
+} else {
+    document.addEventListener("DOMContentLoaded", app.init);
+}
